05ead9c20e5d6db4d857a5ab6dc0f3b956f6b49f
Narawish Sangsiriwut <narawish.s@ku.th>
2023-02-06 13:49:31 +0700

    movement bug fixed

--- Assets/Script/Location.cs
+++ Assets/Script/Location.cs
@@ -8,19 +8,28 @@ namespace Script
     public class Location : MonoBehaviour
     {
         public Timer timer;
+
         private void OnMouseDown()
         {
             switch (GameManager.Instance.state)
             {
                 case GameState.P1Turn:
-                    GameManager.Instance.player1.transform.DOMove(transform.position, 0.5f).SetEase(Ease.InOutQuad);
-                    Debug.Log("Do action: Player DOMove");
-                    timer.DecreaseTime(2);
+                    if (GameManager.Instance.player1.transform.position != transform.position)
+                    {
+                        GameManager.Instance.player1.transform.DOMove(transform.position, 0.5f).SetEase(Ease.InOutQuad);
+                        Debug.Log("Do action: Player DOMove");
+                        timer.DecreaseTime(2);
+                    }
+
                     break;
                 case GameState.P2Turn:
-                    GameManager.Instance.player2.transform.DOMove(transform.position, 0.5f).SetEase(Ease.InOutQuad);
-                    Debug.Log("Do action: Player DOMove");
-                    timer.DecreaseTime(2);
+                    if (GameManager.Instance.player2.transform.position != transform.position)
+                    {
+                        GameManager.Instance.player2.transform.DOMove(transform.position, 0.5f).SetEase(Ease.InOutQuad);
+                        Debug.Log("Do action: Player DOMove");
+                        timer.DecreaseTime(2);
+                    }
+
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
